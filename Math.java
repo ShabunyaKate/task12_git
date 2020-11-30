76c4066f2399925bf14139497e32f86795f4be72
Katerina Shabunia <Katsiaryna_Shabunia@epam.com>
2020-11-30 23:31:03 +0300

    T-10: Split in one commit

--- Math.java
+++ Math.java
@@ -2,4 +2,21 @@ public class Math{
 	public int add(int a, int b){
 		return a+b;
 	}
+	public int sub(int a, int b){
+		return a-b;
+	}
+	public int div(int a, int b){
+		try{
+			return a/b;
+		}
+		catch (Exception e){
+			return 0;
+		}
+	}
+	public int mult(int a, int b){
+		return a*b;
+	}
+	public double sqrt(double a){
+		return Math.sqrt(a);
+	}
 }
